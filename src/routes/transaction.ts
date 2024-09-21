268384aa0635e2cd2e80422e798f673a3471e67e
Rodrigo Aguiar <rodsilva.aguiar@gmail.com>
2024-09-20 22:46:40 -0300

    Aula: Listagem das transações

--- src/routes/transaction.ts
+++ src/routes/transaction.ts
@@ -4,6 +4,23 @@ import { z } from 'zod'
 import { knex } from '../database'
 
 export async function trasactionsRoutes(app: FastifyInstance) {
+  app.get('/', async () => {
+    const transactions = await knex('transactions').select()
+
+    return { transactions }
+  })
+
+  app.get('/:id', async (request) => {
+    const getTransactionParamsSchema = z.object({
+      id: z.string().uuid(),
+    })
+    const { id } = getTransactionParamsSchema.parse(request.params)
+
+    const transaction = await knex('transactions').where('id', id).first()
+
+    return { transaction }
+  })
+
   app.post('/', async (request, reply) => {
     const createTransactionSchema = z.object({
       title: z.string(),
